bc420832219fd6714583908e08106432dd3f8bbe
Cao Yuhao  曹昱豪 <1174478646@qq.com>
2016-04-26 00:07:48 +0800

    aaa

--- JIMI.LA.INDEX/js/index/index.js
+++ JIMI.LA.INDEX/js/index/index.js
@@ -1,137 +1,146 @@
 $(function () {
-    //#3881e0
-    //窗口宽高
-    var winH = $(window).height();
-    var winW = $(window).width();
-    var pageIndex = 2//当前页面（初始化用） 这个变量必须暴露给全局因为其他组件的运动与这个变量有关
-    //window.pageIndex = pageIndex; //但是这是值类型无法传递指针
+        //#3881e0
+        //窗口宽高
+        var winH = $(window).height();
+        var winW = $(window).width();
+        var pageIndex = 0//当前页面（初始化用）
 
-    var isConVelocited=false;
-    //操作的jq对象
-    var $cirLis = $('#circles ul li');
-    var $con = $('#container');
-    var $pages = $con.find('.page');
+        var isConVelocited = false;
+        //操作的jq对象
+        var $cirLis = $('#circles ul li');
+        var $con = $('#container');
+        var $pages = $con.find('.page');
+        var pageNum = $pages.length - 1;//最大索引数所以-1
 
-    //jqPage对象
-    var $page0 = $pages.eq(0);
-    var $page1 = $pages.eq(1);
-    var $page2 = $pages.eq(2);
-    var $page3 = $pages.eq(3);
-    var $page4 = $pages.eq(4);
+        //jqPage对象
+        var $page0 = $pages.eq(0);
+        var $page1 = $pages.eq(1);
+        var $page2 = $pages.eq(2);
+        var $page3 = $pages.eq(3);
+        var $page4 = $pages.eq(4);
 
-    //page0 jq对象
-    var $scroll = $page0.find('.scroll');
-
-
-    //添加背景颜色
-    var colorArr = ['#fff', '#fff', '#fff', 'white', 'white'];
-    $pages.each(function (i, e) {
-        $(this).css('backgroundColor', colorArr[i]);
-    })
+        //page0 jq对象
+        var $scroll = $page0.find('.scroll');
 
 
-    //pageChangeLengthArr是除了首页后面每一页应该增加的高度
-    var pageChangeLengthArr = [winH, winH, winH, 200]; //4页
-    var pageNum = pageChangeLengthArr.length;//4
-
-    //求出每一页应该在的top值
-    var pageTopValueArr = [0];//记录每一页顶部高度的数组
-    for (var i = 0; i < pageNum; i++) { //不-1
-        pageTopValueArr.push(pageTopValueArr[i] + pageChangeLengthArr[i]);
-    }
-
-    //init.............................................................
-    $con.animate({'top': -pageTopValueArr[pageIndex]}, 0);
-    $cirLis.eq(pageIndex).addClass('cur').siblings().removeClass('cur')
+        //添加背景颜色
+        var colorArr = ['#fff', '#fff', '#fff', 'white', 'white'];
+        $pages.each(function (i, e) {
+            $(this).css('backgroundColor', colorArr[i]);
+        })
 
 
-    //Events....................................................................
-    $(window).mousewheel(function (e, delta) {
-        var e = e || event;
-        e.preventDefault();	//阻止页面的默认滚动。
+        //init.............................................................
+        $cirLis.eq(pageIndex).addClass('cur').siblings().removeClass('cur');
+        $pages.eq(pageIndex).css({top: 0}).siblings().css({top: '100%'})
 
 
-        //e.target是事件环的第一环
-        if ($.contains($scroll[0], e.target)) {
-            //jQuery.contains(document.documentElement, document.body); // true
-            return;
-        }
-
-        if (!isConVelocited) {
-
-            var oldIndex = pageIndex;
-            if (delta == -1) {
-                pageIndex++;
-            }
-            else if (delta == 1) {
-                pageIndex--;
-            }
-            pageIndex = pageIndex > pageNum ? pageNum : pageIndex;//验收
-            pageIndex = pageIndex < 0 ? 0 : pageIndex;
-
-            DoPageChange(oldIndex, pageIndex);
-        }
-    })
-
-    $(window).keydown(function (e) {
+        //Events....................................................................
+        $(window).mousewheel(function (e, delta) {
             var e = e || event;
+            e.preventDefault();	//阻止页面的默认滚动。
+
+
+            //e.target是事件环的第一环
+            if ($.contains($scroll[0], e.target)) {
+                return;
+            }
+
             if (!isConVelocited) {
 
                 var oldIndex = pageIndex;
-
-                if (e.keyCode == 38 || e.keyCode == 40) {
-                    e.preventDefault();	//阻止页面的默认滚动。
-                    if (e.keyCode == 38) {
-                        pageIndex--;
-                    }
-                    else if (e.keyCode == 40) {
-                        pageIndex++;
-                    }
-                    pageIndex = pageIndex > pageNum ? pageNum : pageIndex;//验收
-                    pageIndex = pageIndex < 0 ? 0 : pageIndex;
-
-                    DoPageChange(oldIndex, pageIndex);
+                if (delta == -1) {
+                    pageIndex++;
                 }
+                else if (delta == 1) {
+                    pageIndex--;
+                }
+                pageIndex = pageIndex > pageNum ? pageNum : pageIndex;//验收
+                pageIndex = pageIndex < 0 ? 0 : pageIndex;
 
+                DoPageChange(oldIndex, pageIndex);
             }
-        }
-    )
-
-
-    $cirLis.each(function (i, e) {
-        $(this).click(function () {
-            var oldIndex = pageIndex;
-            pageIndex = i;
-            DoPageChange(oldIndex, pageIndex);
         })
-    })
 
-    //functions.......................................................
-    function DoPageChange(oldIndex, pageIndex) {
-        if (oldIndex == pageIndex) {
-            return;
+        $(window).keydown(function (e) {
+                var e = e || event;
+                if (!isConVelocited) {
+
+                    var oldIndex = pageIndex;
+
+                    if (e.keyCode == 38 || e.keyCode == 40) {
+                        e.preventDefault();	//阻止页面的默认滚动。
+                        if (e.keyCode == 38) {
+                            pageIndex--;
+                        }
+                        else if (e.keyCode == 40) {
+                            pageIndex++;
+                        }
+                        pageIndex = pageIndex > pageNum ? pageNum : pageIndex;//验收
+                        pageIndex = pageIndex < 0 ? 0 : pageIndex;
+
+                        DoPageChange(oldIndex, pageIndex);
+                    }
+                }
+            }
+        )
+
+
+        $cirLis.each(function (i, e) {
+            $(this).click(function () {
+                var oldIndex = pageIndex;
+                pageIndex = i;
+                DoPageChange(oldIndex, pageIndex);
+            })
+        })
+
+        //functions.......................................................
+        function DoPageChange(oldIndex, pageIndex) {
+            if (oldIndex == pageIndex) {
+                return;
+            }
+
+
+            //页数不同说明可以移动了
+            isConVelocited = true;
+
+
+            //判断上移还是下移
+            //上移的话 当前到-100% 下移的话 当前到100%
+            var dir=(oldIndex<pageIndex)?'up':'down';
+            var dirJSON={
+                //'up':['-100%','100%'],
+                'up':['-300','300'],
+                //'down':['100%','-100%'],
+                'down':['300','-300'],
+            }
+
+
+            $pages.eq(oldIndex).velocity({'top': '-100%'}, 1000, 'easeInQuart', function () {
+                isConVelocited = false;
+            });
+
+            $pages.eq(pageIndex).velocity({top:"100%"}, 0).velocity({'top': 0}, 1000, 'easeInQuart', function () {
+                isConVelocited = false;
+            });
+
+            var cirArr = [0, 1, 2, 3, 3];//
+            $cirLis.eq(cirArr[pageIndex]).addClass('cur').siblings().removeClass('cur');
+
+            if (oldIndex != pageNum) {
+                AnimateInArr[pageIndex]();
+                AnimateOutArr[oldIndex]();
+            }
+
         }
-        isConVelocited=true;
-        $con.velocity({'top': -pageTopValueArr[pageIndex]}, 800, 'easeInQuart',function(){
-            isConVelocited=false;
-        });
 
-        var cirArr = [0, 1, 2, 3, 3];//
-        $cirLis.eq(cirArr[pageIndex]).addClass('cur').siblings().removeClass('cur');
-
-        if (oldIndex != 4) {
-            AnimateInArr[pageIndex]();
-            AnimateOutArr[oldIndex]();
-        }
-
-    }
         //AnimateJSON.................................................................
         var AnimateInArr = [
             function () {
                 var total = 1200;
                 $page0.find('.eye').velocity({'top': '200%'}, 0).delay(0).velocity({'top': '50%'}, (total), 'ease');
                 $page0.find('.scroll').velocity({'top': '200%'}, 0).delay(0).velocity({'top': '50%'}, (total + 400), 'ease');
-                $page0.find('.title').velocity({'top': '200%'}, 0).delay(0).velocity({'top': '50%'}, (total+200), 'ease');
+                $page0.find('.title').velocity({'top': '200%'}, 0).delay(0).velocity({'top': '50%'}, (total + 200), 'ease');
                 $page0.find('.btnIOS').velocity({'top': '200%'}, 0).delay(0).velocity({'top': '50%'}, (total + 400), 'ease');
                 $page0.find('.btnAZ').velocity({'top': '200%'}, 0).delay(0).velocity({'top': '50%'}, (total + 600), 'ease');
 
@@ -227,5 +236,4 @@ $(function () {
         ];
 
     }
-
-    )
+)
