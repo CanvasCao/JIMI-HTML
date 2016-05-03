6ce4f7e1cc59650b50cfc4498d5b289dc746b895
Cao Yuhao  曹昱豪 <1174478646@qq.com>
2016-05-03 22:20:55 +0800

    aaa

---
+++ JIMI.PRO.INDEX/Module/Header/js/header.js
@@ -0,0 +1,43 @@
+/*!
+ * JimiSdk, a JavaScriptPlugIn v1.0.0
+ * http://www.jimi.la/
+ *
+ * Copyright 2016, CaoYuhao
+ * All rights reserved.
+ * Date: 2016-4-8 10:08:28
+ */
+
+;
+(function (w, d,$, undefined) {
+    function CAOOOOOO(container, data) {
+        this.container = container;
+        this.data = data;
+        this.config = {};
+        this.init();
+    }
+
+    CAOOOOOO.prototype = {
+        init: function () {
+            this.initConfig();
+            this.createDom();
+            this.initCSS();
+            this.bindEvent();
+        },
+        initConfig: function () {
+
+        },
+        createDom: function () {
+
+        },
+        initCSS: function () {
+            var that=this;
+
+        },
+        bindEvent: function () {
+            var that=this;
+        }
+    }
+    w.CAOOOOOO = CAOOOOOO;
+})(window, document,jQuery)
+
+
