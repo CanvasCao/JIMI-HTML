a6e7ccea8c40d0707840075a7ccfa89aa1945435
Cao Yuhao  曹昱豪 <1174478646@qq.com>
2016-05-06 14:25:56 +0800

    aaa

---
+++ bulletcommentaries/增加弹幕原型/增加弹幕原型/files/页面_1/data.js
@@ -0,0 +1,36 @@
+﻿$axure.loadCurrentPage({
+  "url":"页面_1.html",
+  "generationDate":new Date(1462515370651.07),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"36db31414a1449eaa9ae3b122feb914f",
+    "type":"Axure:Page",
+    "name":"页面 1",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"应用字体",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[]}},
+  "masters":{
+},
+  "objectPaths":{
+}});
