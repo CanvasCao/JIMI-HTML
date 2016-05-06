e586ffab45e3d99ab9deb549af03bff8b66dbfc3
Cao Yuhao  曹昱豪 <1174478646@qq.com>
2016-05-06 18:11:58 +0800

    aaa

--- JIMI jQueryPlugIn/FixRate/jQuery-jimiFixRate-1.0.0.js
+++ JIMI jQueryPlugIn/FixRate/jQuery-jimiFixRate-1.0.0.js
@@ -1,18 +1,19 @@
 ;
 (function ($) {
-    $.fn.jimiFixedRate = function () {
+    $.fn.jimiFixedRate = function (standardWidth) {
+        var standardWidth=standardWidth||375;
         var that=this;
 
         var winH=$(window).height();
         var winW=$(window).width();
-        var RATE = winW / 375;
+        var RATE = winW / standardWidth;
 
 
         var h = parseFloat(that.css('height')) * RATE;
         var w = parseFloat(that.css('width')) * RATE;
 
 
-        that.css({height: h, width: w});
+        that.css({height: h, width: w}).css({'line-height':h+'px','font-size':h*0.6+'px'});
 
         return this;
     }
