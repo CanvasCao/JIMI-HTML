f80aaad50e469cf0214d25ad110bdd24bdf16c4a
Cao Yuhao  曹昱豪 <1174478646@qq.com>
2016-06-21 20:10:34 +0800

    aaa

--- LightUp/InfomationPostWithTitleRadius 5-23/js/jimi-prototype.js
+++ LightUp/InfomationPostWithTitleRadius 5-23/js/jimi-prototype.js
@@ -26,15 +26,3 @@ String.prototype.AppendSpansByPeriod = function () {
         return str;
     }
 }
-
-String.prototype.searchToJson = function () {
-    var search = window.location.search.replace('?', "");
-    var kvArr = search.split('&');
-    var finalJson = {}
-    for (i = 0; i < kvArr.length; i++) {
-        var kvSplit = kvArr[i].split('=');
-        finalJson[kvSplit[0].toLowerCase()] = decodeURI(kvSplit[1]);
-
-    }
-    return finalJson;
-}
