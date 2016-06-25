a5c77ddb4be0ce83651e854c559f43a6be019649
Cao Yuhao  曹昱豪 <1174478646@qq.com>
2016-06-25 20:59:36 +0800

    aaa

--- JIMI jQueryPlugIn/Scroll/scrollFixed/jquery-jimiScrollFixed.js
+++ JIMI jQueryPlugIn/Scroll/scrollFixed/jquery-jimiScrollFixed.js
@@ -2,29 +2,26 @@
 (function ($) {
     $.fn.jimiScrollFixed = function (json) {
         var that = this;
-        var fixedMarginTop = json.fixedMarginTop || 15; //当我fixed的时候
-        var $html = $('html');
-        var $body = $('body');
+        var fixedMarginTop=json.fixedMarginTop||15;
+        var $html=$('html');
+        var $body=$('body');
 
-        //获得当前元素距离 顶部和左侧的距离
-        //因为当元素基于窗口fixed的时候 top值已知是15 left就是一开始的元素基于左侧的距离
         var fixedTop = $(this[0]).offset().top;
         var fixedLeft = $(this[0]).offset().left;
-
-        //保存需要fixed的元素的本来是哪种文档流
         var oriPos = that.css('position');
-
         $(window).scroll(function () {
-            //不断获得当前body滚上去多少
+            checkFixed();
+        });
 
-            //window上滚的值 IE和谷歌分别只有body或html有值
-            var winScrollTop = $body.scrollTop() || $html.scrollTop();
 
-            //滚动值+15
-            if (winScrollTop + fixedMarginTop >= fixedTop) {
+        checkFixed();
+        function checkFixed(){
+            var winScrollTop = $body.scrollTop()||$html.scrollTop();
+            if (winScrollTop+fixedMarginTop >= fixedTop) {
                 that.css({
                     'position': 'fixed',
                     top: fixedMarginTop,
+                    //left: fixedLeft
                 })
             }
             else {
@@ -41,7 +38,7 @@
                 }
 
             }
-        })
+        }
         return this;
     }
 })(jQuery);
--- JIMI jQueryPlugIn/Scroll/scrollTo/jquery-jimiScrollTo-2.0.0.js
+++ JIMI jQueryPlugIn/Scroll/scrollTo/jquery-jimiScrollTo-2.0.0.js
@@ -1,58 +1,62 @@
 ;
-//2.0.0可以给$()对象增加marginTop适配器 或者加减className了
 (function ($) {
-    //this是按钮集合 jqObj是按钮点击移动到jqObj顶部
+    //this�ǰ�ť���� jqObj�ǰ�ť����ƶ���jqObj����
     $.fn.jimiScrollTo = function (jqObj, sourceJson, curBtnClassName) {
         var that = this;
-        //这一步相当于数据的适配器
-        var json = {marginTop: 15};
+        //��һ���൱�����ݵ�������
+        var json = {marginTop: 105};
         for (k in sourceJson) {
             json[k] = sourceJson[k];
         }
 
         //console.log(json)
 
-        //右侧滚动位置数组
         var scrollTopArr = [];
         for (i = 0; i < jqObj.length; i++) {
-            scrollTopArr.push($(jqObj[i]).offset().top - json.marginTop);//滚动位置减去15是为了稍微往上滚一点 这样右侧就会稍微偏下一点
+            scrollTopArr.push($(jqObj[i]).offset().top - json.marginTop);
         }
 
-        //滚动区间数组 数量=右侧滚动位置数组+1
         var intervalArr = [];
         intervalArr.push([0, scrollTopArr[0]]);
         for (i = 0; i < scrollTopArr.length - 1; i++) {
             intervalArr.push([scrollTopArr[i], scrollTopArr[i + 1]]);
         }
-        //最后加入最后一个到正无穷区间
-        intervalArr.push([scrollTopArr[i], Number.MAX_VALUE]);
-
-
-        //scrollTopArr是给click事件对齐用的
+        intervalArr.push([scrollTopArr[scrollTopArr.length - 1], Number.MAX_VALUE]);
+        //console.log(intervalArr)
         $(this).click(function () {
             var index = $(this).index();
-            $('html,body').animate({'scrollTop': scrollTopArr[index]}, 'slow', 'swing')
+            //console.log(index);
+            //console.log(scrollTopArr[index])
+            $('html,body').animate({'scrollTop': scrollTopArr[index]}, 'slow', 'swing', function () {
+                checkAlign();
+            })
         })
 
 
-        //intervalArr是给scroll事件对齐用的
         $(window).scroll(function () {
             checkAlign();
         })
 
-        //init................................................
-        checkAlign();
-
+        //init..................................................
+        checkAlign()
         function checkAlign() {
-            //有ClassName我判断滚动位置再加减className
             if (curBtnClassName) {
                 var curScrollTop = $('html').scrollTop() || $('body').scrollTop();
                 for (i = 0; i < intervalArr.length; i++) {
                     if (curScrollTop >= intervalArr[i][0] && curScrollTop <= intervalArr[i][1]) {
-                        $(that).eq(i).addClass(curBtnClassName).siblings().removeClass(curBtnClassName);
+                        var laibuji = i;
+                        //console.log(laibuji)
+                        laibuji = (laibuji == (intervalArr.length - 1)) ? intervalArr.length - 2 : laibuji;
+                        //console.log(laibuji)
+
+                        $(that).eq(laibuji).addClass(curBtnClassName).siblings().removeClass(curBtnClassName);
+                        //console.log(i);
+                        $(that).find('.tri').hide();
+                        $(that).find('.tri').eq(laibuji).show();
+                        //console.log(i)
+                        return;
                     }
                 }
-
             }
         }
 
